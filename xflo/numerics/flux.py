Add Green Gauss gradient computation. Add JST scheme. Fix LF scheme. Adapt data structure.

xflo/numerics/flux.py
@@ -26,21 +26,27 @@ class Flux:
     def __init__(self, fluid):
         self._flu = fluid
 
-    def compute_residual(self, state0, state1, n, l):
+    def compute_residual(self, s0, s1, ds0, ds1, n, l, d):
         """Compute and integrate the flux projected on the edge normal
 
         Parameters:
-        state0 : np.array(4)
-            Conservatives variables in the first cell linked to the face
-        state1 : np.array(4)
-            Conservatives variables in the second cell linked to the face
+        s0 : np.array(4)
+            Conservatives variables in the edge's owner cell
+        s1 : np.array(4)
+            Conservatives variables in the edge's neighboor cell
+        ds0 : np.array(4)
+            Gradient of conservatives variables in the edge's owner cell
+        ds1 : np.array(4)
+            Gradient of conservatives variables in the edge's neighboor cell
         n : np.array(2)
             Edge unit normal vector
         l : float
             Edge length
+        d : float
+            Distance between owner and neighboor cells linked to edge
 
         Returns:
         flux_proj : np.array(4)
-            Numerical flux projected on the face normal direction
+            Integrated flux projected on the edge normal direction
         """
         raise XFloNotImplemented('Flux not implemented!')

xflo/numerics/fv_discretization.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from .gradient import Gradient
 import numpy as np
 
 class FiniteVolume:
@@ -24,8 +25,6 @@ class FiniteVolume:
         Problem definition
     _flx : Flux object
         Flux formulation
-    _nvar_cell : int
-        Number of variables (unknown) per cell
     _ncells : int
         Number of cells
     _states : np.array(float)
@@ -35,10 +34,11 @@ class FiniteVolume:
     _dt : np.array(float)
         Local time step divided by cell area
     """
-    def __init__(self, problem, flux):
+    def __init__(self, problem, flux, gradient=None):
         # Data objetcs
         self.problem = problem
         self._flx = flux
+        self._grd = gradient if gradient is not None else Gradient(problem)
 
         # Set sizes
         self._ncells = self.problem.mesh.get_ncells() # number of cells
@@ -71,7 +71,7 @@ class FiniteVolume:
 
         # Update residuals and problem
         self._compute_residuals()
-        self.problem.update(self._states, self._residuals, self._get_uids)
+        self.problem.update(self._states, self._residuals)
 
     def update(self, states):
         """Set states and update problem
@@ -99,7 +99,6 @@ class FiniteVolume:
         u = self.problem.get_variables('VelocityX')
         v = self.problem.get_variables('VelocityY')
         c = self.problem.get_variables('SpeedOfSound')
-        dt_a = np.zeros(self._ncells, dtype=float)
         for i_cell in range(self._ncells):
             a = np.linalg.norm(np.array([u[i_cell], v[i_cell]])) + c[i_cell] # wave speed
             self._dt[self.problem.get_uids(i_cell)] = cfl * np.sqrt(area[i_cell]) / a / area[i_cell]
@@ -113,6 +112,9 @@ class FiniteVolume:
         # Reset residuals
         self._residuals[:] = 0.
 
+        # Compute gradients
+        grads = self._grd.compute(self._states)
+
         # Get edge to cell connectivity
         cids = self.problem.mesh.get_edges_cells()
 
@@ -122,18 +124,20 @@ class FiniteVolume:
             bnd = self.problem.mesh.get_boundary(bc.get_name())
             eids = bnd.get_edge_ids()
             lgts, nrms = bnd.get_edge_metrics()
+            dsts = bnd.get_cell_distances()
             for i_edge, eid in enumerate(eids):
                 # neighboor cell and corresponding unknown indices
                 cid = cids[eid][0]
                 sid = self.problem.get_uids(cid)
                 # integrated projected flux
                 ghost_state = bc.compute_ghost(self._states[sid], nrms[i_edge])
-                self._residuals[sid] += self._flx.compute_residual(self._states[sid], ghost_state, nrms[i_edge], lgts[i_edge])
+                self._residuals[sid] += self._flx.compute_residual(self._states[sid], ghost_state, grads[sid], grads[sid], nrms[i_edge], lgts[i_edge], dsts[i_edge])
 
         # Compute flux on internal edges and add residuals to cells
         fld = self.problem.mesh.get_field()
         eids = fld.get_edge_ids()
         lgts, nrms = fld.get_edge_metrics()
+        dsts = fld.get_cell_distances()
         for i_edge, eid in enumerate(eids):
             # "left" and "right" neighboor cells
             cid0 = cids[eid][0]
@@ -142,6 +146,6 @@ class FiniteVolume:
             sid0 = self.problem.get_uids(cid0)
             sid1 = self.problem.get_uids(cid1)
             # integrated projected flux
-            ipflx = self._flx.compute_residual(self._states[sid0], self._states[sid1], nrms[i_edge], lgts[i_edge])
+            ipflx = self._flx.compute_residual(self._states[sid0], self._states[sid1], grads[sid0], grads[sid1], nrms[i_edge], lgts[i_edge], dsts[i_edge])
             self._residuals[sid0] += ipflx
             self._residuals[sid1] -= ipflx

xflo/structure/group.py
@@ -28,8 +28,10 @@ class Group:
         Edges length
     _enrm : np.array(float), size: (n_edges, 2)
         Edges unit normal vector
+    _edst : np.array(float), size: (n_edges)
+        Distance between neighboor cells
     """
-    def __init__(self, name, edge_ids, edge_vertices, vertices, sort=False):
+    def __init__(self, name, edge_ids, edge_vertices, edge_cells, cell_vertices, vertices, sort=False):
         """Parameters:
         name : str
             Name of the group
@@ -37,6 +39,10 @@ class Group:
             Group edges ID
         edge_vertices : list(list(int))
             Edges to vertices connectivity
+        edge_cells : np.array(np.array(int)), size: (n_edges, n_cells_edge)
+            Edges to cells connectivity
+        cell_vertices : np.array(np.array(int)), size: (n_cells, n_nodes_cell)
+            Cells to vertices connectivity
         vertices : np.array(float), size: (n_nodes, 2)
             Coordinates of the mesh vertices
         sort : bool
@@ -60,14 +66,23 @@ class Group:
         else:
             self._eids = edge_ids
 
-        # Compute edges length and unit normal vector
+        # Compute owner cell CG
+        cg = np.zeros((n_edges, 2), dtype=float)
+        for i_edge in range(n_edges):
+            for vid in cell_vertices[edge_cells[self._eids[i_edge]][0]]:
+                cg[i_edge] += vertices[vid]
+            cg[i_edge] /= cell_vertices[edge_cells[self._eids[i_edge]][0]].shape[0]
+
+        # Compute edges length, unit normal vector and cell-cell distance
         self._elgt = np.zeros(n_edges, dtype=float)
         self._enrm = np.zeros((n_edges, 2), dtype=float)
+        self._edst = np.zeros(n_edges, dtype=float)
         for i_edge in range(n_edges):
             xy0 = vertices[edge_vertices[self._eids[i_edge]][0]]
             xy1 = vertices[edge_vertices[self._eids[i_edge]][1]]
             self._elgt[i_edge] = np.linalg.norm(xy1 - xy0)
             self._enrm[i_edge, :] = np.array([-(xy1[1] - xy0[1]), xy1[0] - xy0[0]]) / self._elgt[i_edge]
+            self._edst[i_edge] = 2 * np.linalg.norm(0.5 * (xy0 + xy1) - cg[i_edge])
 
     def get_edge_ids(self):
         """Return:
@@ -81,3 +96,9 @@ class Group:
         group edges unit normal vector : np.array(float)
         """
         return self._elgt, self._enrm
+
+    def get_cell_distances(self):
+        """Returns:
+        group edges inter-cell distance : np.array(float)
+        """
+        return self._edst

xflo/structure/mesh.py
@@ -23,8 +23,6 @@ class Mesh:
     Attributes:
     _name : str
         Name of the mesh
-    _ncells : int
-        Number of field cells
     _verts : np.array(float), size: (n_nodes, 2)
         Coordinates of the mesh vertices
     _cells : np.array(np.array(int)), size: (n_cells, n_nodes_cell)
@@ -59,17 +57,16 @@ class Mesh:
         """
         # Init data structure
         self._name = name
-        self._ncells = len(cells)
         self._verts = np.array(vertices, dtype=float)
         self._cells = np.array([np.array(cell, dtype=int) for cell in cells], dtype=np.ndarray)
         self._edg_cel = np.array([np.array(edge, dtype=int) for edge in edges_cells], dtype=np.ndarray)
         self._edg_vrt = np.array(edge_vertices, dtype=int)
 
         # Create groups
-        self._fld = Group('field', field_edges, edge_vertices, self._verts)
+        self._fld = Group('field', field_edges, edge_vertices, self._edg_cel, self._cells, self._verts)
         self._bnd = {}
         for bnd_name, bnd_edgs in boundary_edges.items():
-            self._bnd[bnd_name] = Group(bnd_name, bnd_edgs, edge_vertices, self._verts, sort=True)
+            self._bnd[bnd_name] = Group(bnd_name, bnd_edgs, edge_vertices, self._edg_cel, self._cells, self._verts, sort=True)
 
         # Compute cells area
         self._carea = np.zeros(self._cells.shape[0])
@@ -91,7 +88,13 @@ class Mesh:
         """Returns:
         number of cells : int
         """
-        return self._ncells
+        return self._cells.shape[0]
+
+    def get_nedges(self):
+        """Returns:
+        number of edges : int
+        """
+        return self._edg_cel.shape[0]
 
     def get_vertices(self):
         """Returns: